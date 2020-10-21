c8d66bdd0a688cd1d81210987b11d8c266cd0849
Dhiana <dhianaslv@outlook.com>
2020-10-21 18:45:09 -0300

    linear regression

---
+++ public/linear_regression.js
@@ -0,0 +1,61 @@
+var x = [];
+var y = [];
+var prediction = [];
+
+function removeUndefined(array) {
+  for (let i = 0; i <= array.length; ++i) {
+    if (typeof array[i] === "undefined") {
+      return array.splice(i, array.length);
+    }
+  }
+}
+
+function readSpreadsheet() {
+  const xlsx = require("xlsx");
+  const wb = xlsx.readFile("spreadsheet.xlsx", { cellDates: true });
+  const ws = wb.Sheets["Página1"];
+
+  const data = xlsx.utils.sheet_to_json(ws);
+
+  x = data.map((record) => {
+    return record.newCases;
+  });
+
+  y = data.map((record) => {
+    return record.newDeaths;
+  });
+
+  prediction = data.map((record) => {
+    return record.validation;
+  });
+
+  removeUndefined(x);
+  removeUndefined(y);
+  removeUndefined(prediction);
+
+  console.log(x);
+  console.log(y);
+  console.log(prediction);
+}
+
+function trainingModel() {
+  const tf = require("@tensorflow/tfjs");
+
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+  model.compile({
+    loss: "meanSquaredError",
+    optimizer: "adam",
+  });
+
+  const xs = tf.tensor2d(x, [x.length, 1]);
+  const ys = tf.tensor2d(y, [y.length, 1]);
+
+  model.fit(xs, ys, { epochs: 2000 }).then(() => {
+    model.predict(tf.tensor2d(prediction, [prediction.length, 1])).print();
+  });
+}
+
+readSpreadsheet();
+trainingModel();
